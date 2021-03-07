727006b383b64f5628d5740c3d9c096c7a78471e
Yadvi Bhalla <yadvibhalla2002@gmail.com>
2021-03-07 10:50:30 +0530

    image added

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-        return "<h3>Hello, Yadvi!</h3>"
+    return "<h3>Hello, Yadvi!</h3>    <img src='https://myimagest.blob.core.windows.net/mycimage/badge.png'/>"
